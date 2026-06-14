77cfc86061b597d9c88b37470820d6a4cab084fb
ivankadyka <ivankadyko@gmail.com>
2026-06-15 00:09:17 +0200

    init SwaggerModule

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,15 +1,30 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
-  );
-  await app.listen(process.env.PORT ?? 8000);
+  )
+
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.redirect('/api');
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API description')
+    .setVersion('0.1')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(process.env.PORT ?? 8000)
 }
-bootstrap();
+bootstrap()
